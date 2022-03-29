4e23e4772a6732db502a0b08bbf7114bb38456b5
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2022-03-29 16:54:53 +0000

    Add for loop for multiple rows

--- mario.c
+++ mario.c
@@ -1,3 +1,27 @@
 #include<stdio.h>
 #include<cs50.h>
 //these are libraries
+
+int main (void)
+{
+    //Get The Height
+    int height = get_int("Enter a height: ");
+    int counter = height;
+    for (int j = 0; j< height; j++)
+        {
+        for (int i = 0; i < height; i++)
+            {
+                if (i<counter -1)
+                    {
+                        printf(" ");
+                    }
+                else
+                    {
+                        printf("#");
+                    }
+            }
+        printf("\n");
+        counter --;
+        }
+    //Created a nested loop to do multiple rows
+}
